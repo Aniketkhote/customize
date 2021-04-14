0b02a9933a6d8ffd7ade6628536945cbe94df58d
unknown <aniketkhote99@gmail.com>
2021-04-14 17:51:48 +0530

    added some list and map methods

--- lib/src/core/extensions/list_extension.dart
+++ lib/src/core/extensions/list_extension.dart
@@ -5,61 +5,205 @@ extension ListExtensionson on List<dynamic> {
   /// Expands each element of this [Iterable] into zero or more elements.
   ///
   ///Merge list of lists into single list
-  List<dynamic> get flatten {
-    return this.expand((dynamic i) => i).toList();
-  }
+  List<dynamic> get flatten => FxList.flatten(this);
 
   /// Expands each element of this [Iterable] into zero or more elements.
   ///
   ///Merge list of maps into single map
-  Map<dynamic, dynamic> get flattenMap {
-    List<dynamic> _mapList = this.expand((dynamic map) => map.entries).toList();
+  Map<dynamic, dynamic> get flattenMap => FxList.flattenMap(this);
+
+  /// Expands each element of this [Iterable] into zero or more elements.
+  ///
+  ///Get values from list of maps either passing key or not
+  List<dynamic> getValues([dynamic key]) => FxList.getValues(this, key);
+
+  ///Get sorted list
+  List<dynamic> sorted([bool isDecending = false]) =>
+      FxList.sorted(this, isDecending);
+
+  ///Creates a new list with the elements of this that are not in other.
+  List<dynamic> diff(List<dynamic> list) => FxList.diff(this, list);
+
+  ///counts the occurrences of values in the list
+  Map<dynamic, dynamic> get countBy => FxList.countBy(this);
+
+  ///Removes elements from the list that do not have a specified item value
+  ///
+  ///that is not contained within the given list
+  List<dynamic> whereIn(dynamic key, List<dynamic> params) =>
+      FxList.whereIn(this, key, params);
+
+  ///Removes elements from the list that have a specified item value
+  ///
+  ///that is not contained within the given list
+  List<dynamic> whereNotIn(dynamic key, List<dynamic> params) =>
+      FxList.whereNotIn(this, key, params);
+
+  /// Filters the collection by determining if a specified item value is within a given range
+  List<dynamic> whereBetween(dynamic key, num start, num end) =>
+      FxList.whereBetween(this, key, start, end);
+
+  ///Filters the collection by determining if a specified item value is outside of a given range
+  List<dynamic> whereNotBetween(dynamic key, num start, num end) =>
+      FxList.whereNotBetween(this, key, start, end);
+
+  ///Get minimum number
+  int get min => FxList.min(this);
+
+  ///Get maximum number
+  num get max => FxList.max(this);
+
+  ///Get sum of numbers
+  num get sum => FxList.sum(this);
+
+  ///Get average of numbers
+  num get avg => FxList.avg(this);
+
+  ///Checks given key is exists or not
+  bool containsInMap(dynamic key, dynamic value) =>
+      FxList.containsInMap(this, key, value);
+
+  ///Checks given key is exists or not
+  bool containsKeyInMap(dynamic key) => FxList.containsKeyInMap(this, key);
+
+  ///Checks given value is exists or not
+  bool containsValueInMap(dynamic value) =>
+      FxList.containsValueInMap(this, value);
+}
+
+class FxList {
+  /// Expands each element of this [Iterable] into zero or more elements.
+  ///
+  ///Merge list of lists into single list
+  static List<dynamic> flatten(List<dynamic> list) =>
+      list.expand((dynamic i) => i).toList();
+
+  /// Expands each element of this [Iterable] into zero or more elements.
+  ///
+  ///Merge list of maps into single map
+  static Map<dynamic, dynamic> flattenMap(List<dynamic> list) {
+    List<dynamic> _mapList = list.expand((dynamic map) => map.entries).toList();
     return Map<dynamic, dynamic>.fromIterable(_mapList,
         key: (dynamic v) => v.key, value: (dynamic v) => v.value);
   }
 
   /// Expands each element of this [Iterable] into zero or more elements.
   ///
-  ///Merge list of maps values into single list
-  List<dynamic> get flattenMapToList {
-    List<dynamic> _list;
-    this.flattenMap.forEach((dynamic key, dynamic value) => _list.add(value));
+  ///Get values from list of maps either passing key or not
+  static List<dynamic> getValues(List<dynamic> list, [dynamic key]) {
+    List<dynamic> _list = <dynamic>[];
+    list.forEach((dynamic element) {
+      if (element.containsKey(key)) {
+        _list?.add(element[key]);
+      } else {
+        element.forEach((dynamic k, dynamic v) => _list?.add(v));
+      }
+    });
     return _list;
   }
 
   ///Get sorted list
-  List<dynamic> sorted([bool isDecending = false]) {
-    this.sort();
+  static List<dynamic> sorted(List<dynamic> list, [bool isDecending = false]) {
+    list.sort();
 
     if (isDecending)
-      return this.reversed.toList();
+      return list.reversed.toList();
     else
-      return this;
+      return list;
+  }
+
+  ///Creates a new list with the elements of this that are not in other.
+  static List<dynamic> diff(List<dynamic> list1, List<dynamic> list2) {
+    list1.removeWhere(
+        (dynamic first) => list2.any((dynamic second) => second == first));
+    return list1;
+  }
+
+  ///counts the occurrences of values in the list
+  static Map<dynamic, dynamic> countBy(List<dynamic> list) {
+    Map<dynamic, dynamic> _map;
+    list.forEach(
+        (dynamic x) => _map[x] = _map.containsKey(x) ? _map[x] + 1 : 1);
+    return _map;
+  }
+
+  ///Removes elements from the list that do not have a specified item value
+  ///
+  ///that is not contained within the given list
+  static List<dynamic> whereIn(
+          List<dynamic> list, dynamic key, List<dynamic> params) =>
+      null;
+
+  ///Removes elements from the list that have a specified item value
+  ///
+  ///that is not contained within the given list
+  static List<dynamic> whereNotIn(
+      List<dynamic> list, dynamic key, List<dynamic> params) {
+    params.forEach((dynamic param) =>
+        list.removeWhere((dynamic map) => FxMap.contains(map, key, param)));
+    return list;
+  }
+
+  /// Filters the collection by determining if a specified item value is within a given range
+  static List<dynamic> whereBetween(
+      List<dynamic> list, dynamic key, num start, num end) {
+    List<dynamic> _list = <dynamic>[];
+
+    list.forEach((dynamic element) {
+      if (element.containsKey(key) && element[key] != null) {
+        if (element[key] > start && element[key] < end) {
+          _list?.add(element);
+        }
+      }
+    });
+
+    return _list;
+  }
+
+  /// Filters the list by determining if a specified item value is outside of a given range
+  static List<dynamic> whereNotBetween(
+      List<dynamic> list, dynamic key, num start, num end) {
+    List<dynamic> _list = <dynamic>[];
+
+    list.forEach((dynamic element) {
+      if (element.containsKey(key) && element[key] != null) {
+        if (element[key] < start || element[key] > end) {
+          _list?.add(element);
+        }
+      }
+    });
+
+    return _list;
   }
 
   ///Get minimum number
-  int get min {
-    this.sort();
-    return this.first;
+  static int min(List<dynamic> list) {
+    list.sort();
+    return list.first;
   }
 
   ///Get maximum number
-  num get max {
-    this.sort();
-    return this.last;
+  static num max(List<dynamic> list) {
+    list.sort();
+    return list.last;
   }
 
   ///Get sum of numbers
-  num get sum => this.cast<num>().reduce((num a, num b) => a + b);
+  static num sum(List<dynamic> list) =>
+      list.cast<num>().reduce((num a, num b) => a + b);
 
   ///Get average of numbers
-  num get avg => this.sum / this.cast<num>().length;
+  static num avg(List<dynamic> list) => list.sum / list.cast<num>().length;
 
   ///Checks given key is exists or not
-  bool containsKeyInMap(dynamic key) =>
-      this.any((dynamic element) => element.containsKey(key));
+  static bool containsInMap(List<dynamic> list, dynamic key, dynamic value) =>
+      list.any((dynamic element) => FxMap.contains(element, key, value));
+
+  ///Checks given key is exists or not
+  static bool containsKeyInMap(List<dynamic> list, dynamic key) =>
+      list.any((dynamic element) => element.containsKey(key));
 
   ///Checks given value is exists or not
-  bool containsValueInMap(dynamic value) =>
-      this.any((dynamic element) => element.containsValue(value));
+  static bool containsValueInMap(List<dynamic> list, dynamic value) =>
+      list.any((dynamic element) => element.containsValue(value));
 }
--- lib/src/core/extensions/map_extension.dart
+++ lib/src/core/extensions/map_extension.dart
@@ -3,29 +3,163 @@ extension MapExtensionson on Map<dynamic, dynamic> {
   ///Whether this map contains the given [key]/[value] pair.
   ///
   ///Returns true if any of the key / value pair in the map are equal.
-  bool containsMap(dynamic key, dynamic value) {
-    this.forEach((dynamic k, dynamic v) {
-      if (k.toString().toLowerCase() == key.toString().toLowerCase() &&
-          v.toString().toLowerCase() == value.toString().toLowerCase())
-        return true;
+  bool contains(dynamic key, dynamic value) => FxMap.contains(this, key, value);
+
+  ///Returns all entries of this map.
+  ///
+  ///if first map keys are not in second map.
+  Map<dynamic, dynamic> diffKeys(Map<dynamic, dynamic> map) =>
+      FxMap.diffKeys(this, map);
+
+  ///Returns all entries of this map.
+  ///
+  ///if first map values are not in second map.
+  Map<dynamic, dynamic> diffValues(Map<dynamic, dynamic> map) =>
+      FxMap.diffValues(this, map);
+
+  /// Reads a [key] value of [bool] type from [Map].
+  ///
+  /// If value/map is NULL or not [bool] type return default value [defaultBool]
+  bool getBool(String key) => FxMap.getBool(this, key);
+
+  /// Reads a [key] value of [int] type from [Map].
+  ///
+  /// If value/map  is NULL or not [int] type return default value [defaultInt]
+  int getInt(String key) => FxMap.getInt(this, key);
+
+  /// Reads a [key] value of [double] type from [Map].
+  ///
+  /// If value/map  is NULL or not [double] type return default value [defaultDouble]
+  double getDouble(String key) => FxMap.getDouble(this, key);
+
+  /// Reads a [key] value of [String] type from [Map].
+  ///
+  /// If value/map  is NULL or not [String] type return default value [defaultString]
+  String getString(String key) => FxMap.getString(this, key);
+
+  /// Reads a [key] value of [List] type from [Map].
+  ///
+  /// If value/map  is NULL or not [List] type return default value [defaultString]
+  List<T> getList<T>(String key) => FxMap.getList(this, key);
+
+  ///Parse an object to int if not valid returns 0
+  int toInt(Object value) => FxMap.toInt(value);
+
+  ///Parse an object to double if not valid returns 0
+  double toDouble(Object value) => FxMap.toDouble(value);
+}
+
+class FxMap {
+  static bool _defaultBool = false;
+  static int _defaultInt = 0;
+  static double _defaultDouble = 0;
+  static String _defaultString = '';
+
+  ///Whether this map contains the given [key]/[value] pair.
+  ///
+  ///Returns true if any of the key / value pair in the map are equal.
+  static bool contains(Map<dynamic, dynamic> map, dynamic key, dynamic value) {
+    bool isContain = false;
+    map.forEach((dynamic k, dynamic v) {
+      if (k == key && v == value) isContain = true;
     });
 
-    return false;
+    return isContain;
   }
 
   ///Returns all entries of this map.
   ///
   ///if first map keys are not in second map.
-  Map<dynamic, dynamic> diffKeys(Map<dynamic, dynamic> map) {
-    this.removeWhere((dynamic key, dynamic value) => map.containsKey(key));
-    return this;
+  static Map<dynamic, dynamic> diffKeys(
+      Map<dynamic, dynamic> map1, Map<dynamic, dynamic> map2) {
+    map1.removeWhere((dynamic key, dynamic value) => map2.containsKey(key));
+    return map1;
   }
 
   ///Returns all entries of this map.
   ///
   ///if first map values are not in second map.
-  Map<dynamic, dynamic> diffValues(Map<dynamic, dynamic> map) {
-    this.removeWhere((dynamic key, dynamic value) => map.containsValue(value));
-    return this;
+  static Map<dynamic, dynamic> diffValues(
+      Map<dynamic, dynamic> map1, Map<dynamic, dynamic> map2) {
+    map1.removeWhere((dynamic key, dynamic value) => map2.containsValue(value));
+    return map1;
+  }
+
+  /// Reads a [key] value of [bool] type from [Map].
+  ///
+  /// If value/map is NULL or not [bool] type return default value [defaultBool]
+  static bool getBool(Map<dynamic, dynamic> map, String key) {
+    if (map == null) map = <dynamic, dynamic>{};
+
+    if (map.containsKey(key)) if (map[key] is bool)
+      return map[key] ?? _defaultBool;
+    return _defaultBool;
+  }
+
+  /// Reads a [key] value of [int] type from [Map].
+  ///
+  /// If value/map  is NULL or not [int] type return default value [defaultInt]
+  static int getInt(Map<dynamic, dynamic> map, String key) {
+    if (map == null) map = <dynamic, dynamic>{};
+
+    if (map.containsKey(key)) return toInt(map[key]);
+    return _defaultInt;
+  }
+
+  /// Reads a [key] value of [double] type from [Map].
+  ///
+  /// If value/map  is NULL or not [double] type return default value [defaultDouble]
+  static double getDouble(Map<dynamic, dynamic> map, String key) {
+    if (map == null) map = <dynamic, dynamic>{};
+
+    if (map.containsKey(key)) return toDouble(map[key]);
+    return _defaultDouble;
+  }
+
+  /// Reads a [key] value of [String] type from [Map].
+  ///
+  /// If value/map  is NULL or not [String] type return default value [defaultString]
+  static String getString(Map<dynamic, dynamic> map, String key) {
+    if (map == null) map = <dynamic, dynamic>{};
+
+    if (map.containsKey(key)) if (map[key] is String)
+      return map[key] ?? _defaultString;
+    return _defaultString;
+  }
+
+  /// Reads a [key] value of [List] type from [Map].
+  ///
+  /// If value/map  is NULL or not [List] type return default value [defaultString]
+  static List<T> getList<T>(Map<dynamic, dynamic> map, String key) {
+    if (map == null) map = <T, T>{};
+
+    if (map.containsKey(key)) if (map[key] is List<T>) return map[key] ?? <T>[];
+    return <T>[];
+  }
+
+  ///Parse an object to int if not valid returns 0
+  static int toInt(Object value) {
+    if (value != null) {
+      try {
+        return int.parse(value);
+      } on Exception catch (e, s) {
+        print("toInt Exception : $e\n$s");
+      }
+    }
+    print("Error in toInt $value");
+    return 0;
+  }
+
+  ///Parse an object to double if not valid returns 0
+  static double toDouble(Object value) {
+    if (value != null) {
+      try {
+        return double.parse(value) ?? 0.0;
+      } on Exception catch (e, s) {
+        print("toDouble Exception : $e\n$s");
+      }
+    }
+    print("Error in toDouble $value");
+    return 0;
   }
 }
